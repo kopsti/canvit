d12ce4620de466ae5a89075f004ce2cc94c38c4c
Timos Kopsidas <kopsidas.t@gmail.com>
2017-10-09 14:14:47 +0300

    Organize head

--- src/canvit/context_processors.py
+++ src/canvit/context_processors.py
@@ -7,10 +7,14 @@ def current_site(request):
         current_site = Site.objects.get_current()
         site_title = Site.objects.get_current().site_id.title
         site_description = Site.objects.get_current().site_id.description
+        site_logo = Site.objects.get_current().site_id.logo
+        site_favicon = Site.objects.get_current().site_id.favicon
         return {
             'current_site': current_site,
             'site_title': site_title,
-            'site_description': site_description
+            'site_description': site_description,
+            'site_logo': site_logo,
+            'site_favicon': site_favicon,
         }
     except Site.DoesNotExist:
         # always return a dict, no matter what!
